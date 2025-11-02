50815b21a4a257261d6581bbf619e75a95a1ad06
Priyank Saxena <168936117+PriyankSaxena01@users.noreply.github.com>
2025-11-02 19:39:56 +0530

    Create #countnoofwords.cpp

---
+++ string 1/#countnoofwords.cpp
@@ -0,0 +1,36 @@
+
+
+// GFG EASY
+
+int countWords(string s) {
+    int wordCount = 0;
+    bool inWord = false;
+
+    for (int i = 0; i < s.length(); ++i) {
+
+        if (s[i] == ' ') {
+            inWord = false;
+        } 
+        else if (s[i] == '\\') {
+            if (i + 1 < s.length() && (s[i + 1] == 'n' || s[i + 1] == 't')) {
+                inWord = false;
+                i++; 
+            }
+            else {
+                if (!inWord) {
+                    wordCount++;
+                    inWord = true; 
+                }
+            }
+        }
+
+        else {
+            if (!inWord) {
+                wordCount++;  
+                inWord = true; 
+            }
+        }
+    }
+    
+    return wordCount;
+}
