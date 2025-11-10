0caa11ad8689170bf843f1bffbcf655fcc474f95
Priyank Saxena <168936117+PriyankSaxena01@users.noreply.github.com>
2025-11-10 23:05:22 +0530

    Add files via upload

---
+++ Advanced Array I/best_time_to_buy_and_sell_stocks.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxprice=INT_MIN;
+        int minprice=INT_MAX;
+
+        for(int i=0;i<prices.size();i++)
+        {
+            minprice=min(minprice,prices[i]);
+            maxprice=max(maxprice,prices[i]-minprice);
+        }
+        if(maxprice>0)
+        return maxprice;
+        else
+        return 0;
+    }
+};
