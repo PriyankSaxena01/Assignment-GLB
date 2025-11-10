ad483daa78dddc728b538a3d1d508996467dfdd8
Priyank Saxena <168936117+PriyankSaxena01@users.noreply.github.com>
2025-11-10 23:03:21 +0530

    Create circularqueue.cpp

---
+++ Queue Introduction/circularqueue.cpp
@@ -0,0 +1,104 @@
+class MyCircularQueue {
+    private:
+    struct Node{
+        Node* next;
+        int val;
+        Node(int n)
+        {
+            val=n;
+            next=NULL;
+        }
+    };
+    Node* head;
+    Node* tail;
+    int size;
+    int capacity;
+public:
+    MyCircularQueue(int k) {
+        head=NULL;
+        tail=NULL;
+        size=0;
+        capacity=k;
+    }
+    
+    bool enQueue(int value) {
+        if(isFull()){
+            return false;
+        }
+
+        Node* temp=new Node(value);
+        if(!head)
+        {
+            head=temp;
+            tail=temp;
+            tail->next=head;
+            size++;
+            return true;
+        }
+        else{
+            tail->next=temp;
+            tail=temp;
+            tail->next=head;
+            size++;
+            return true;
+        }
+
+    }
+    
+    bool deQueue() {
+        if(isEmpty())
+        return false;
+        if(size==1)
+        {
+        delete head;
+        head=tail=NULL;
+        size--;
+        return true;
+        }else
+        {
+        Node* temp=head;
+        head=head->next;
+        tail->next=head;
+        delete temp;    
+        size--;
+        return true;
+        } 
+        
+    }
+    
+    int Front() {
+        if(isEmpty())
+        return -1;
+        return head->val;
+    }
+    
+    int Rear() {
+        if(isEmpty())
+        return -1;
+        return tail->val;
+    }
+    
+    bool isEmpty() {
+        if(size==0)
+        return true;
+        return false;
+    }
+    
+    bool isFull() {
+        if(size==capacity)
+        return true;
+        return false;
+        
+    }
+};
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * MyCircularQueue* obj = new MyCircularQueue(k);
+ * bool param_1 = obj->enQueue(value);
+ * bool param_2 = obj->deQueue();
+ * int param_3 = obj->Front();
+ * int param_4 = obj->Rear();
+ * bool param_5 = obj->isEmpty();
+ * bool param_6 = obj->isFull();
+ */
