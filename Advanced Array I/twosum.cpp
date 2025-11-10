fd551fbcc7e889887a4c88baf3283aaf0cdeccf2
Priyank Saxena <168936117+PriyankSaxena01@users.noreply.github.com>
2025-11-10 23:04:42 +0530

    Create twosum.cpp

---
+++ Advanced Array I/twosum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n1,n2;
+        for(int i=0;i<nums.size();i++)
+        {
+            for(int j=i+1;j<nums.size();j++)
+            {
+                if(nums[i]+nums[j]==target)
+                {
+                n1=i;
+                n2=j;
+                break;
+                }
+            }
+        }
+        return {n1,n2};
+    }
+};
