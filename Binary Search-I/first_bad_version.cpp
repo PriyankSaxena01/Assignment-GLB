61d514807da5858084732cf77656221ac084c3cb
Priyank Saxena <168936117+PriyankSaxena01@users.noreply.github.com>
2025-11-10 23:07:51 +0530

    Create first_bad_version.cpp

---
+++ Binary Search-I/first_bad_version.cpp
@@ -0,0 +1,18 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        
+        for(int i=n;i>=0;i--)
+        {
+            if(!isBadVersion(i))
+            {
+                i++;
+                return i;
+            }
+        }
+        return 0;
+    }
+};
