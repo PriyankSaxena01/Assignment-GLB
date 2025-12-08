b3829ca53edd8a2539bac21edbdf5cd6c28a59c2
Priyank Saxena <168936117+PriyankSaxena01@users.noreply.github.com>
2025-12-08 17:05:46 +0530

    Create jumpgame_II.cpp

---
+++ Dynamic Programming V/jumpgame_II.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class JumpGameII {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        int far = 0, end = 0, jumps = 0;
+        for (int i = 0; i < n - 1; i++) {
+            far = max(far, i + nums[i]);
+            if (i == end) {
+                jumps++;
+                end = far;
+            }
+        }
+        return jumps;
+    }
+};
+
+int main() {
+}
