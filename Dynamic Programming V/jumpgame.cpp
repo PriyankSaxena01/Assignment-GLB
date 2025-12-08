457e122d17320e1e12597fdac84f49eade222ea9
Priyank Saxena <168936117+PriyankSaxena01@users.noreply.github.com>
2025-12-08 17:05:13 +0530

    Create jumpgame.cpp

---
+++ Dynamic Programming V/jumpgame.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class JumpGame {
+public:
+    bool canJump(vector<int>& nums) {
+        int boundary = 0;
+        for (int i = 0; i <= boundary; i++) {
+            boundary = max(boundary, i + nums[i]);
+            if (boundary >= nums.size() - 1)
+                return true;
+        }
+        return false;
+    }
+};
+
+int main() {
+}
